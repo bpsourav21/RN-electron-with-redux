0dfb48f262927346857f67e0b3510f7cc51e8e5c
bpsourav21 <bpsourav21@gmail.com>
2018-03-27 15:32:28 +0600

    skip route and privet route working

--- app/Routes.js
+++ app/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { createStore, applyMiddleware, combineReducers, BackHandler } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { Link, withRouter } from "react-router-dom";
-import { Switch, Route } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 //components
 import Home from './components/Home'
 import Table from './components/Table'
@@ -18,23 +18,58 @@ class Routes extends Component {
     this.state = {
     };
   }
+  isAuthenticated() {
+    if (this.props.token) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
   render() {
+    const PrivateRoute = ({ component: Component, ...rest }) => (
+      <Route {...rest} render={props => (
+        this.isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }} />
+          )
+      )} />
+    )
+
+    const SkipRoute = ({ component: Component, ...rest }) => (
+      <Route {...rest} render={props => (
+        this.isAuthenticated() ? (
+          <Redirect to={{
+            pathname: '/',
+            state: { from: props.location }
+          }} />
+
+        ) : (
+            <Component {...props} />
+          )
+      )} />
+    )
     return (
       <Switch>
         <Route path="/settings" component={Settings} />
         <Route path="/reports" component={Reports} />
         <Route path="/items" component={Items} />
-        <Route path="/table" component={Table} />
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Home} />
-        
+        <PrivateRoute path="/table" component={Table} />
+        <SkipRoute path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+
       </Switch>
     )
   }
 }
 const mapStateToProps = (state) => {
   return {
+    token: false //state.auth.token,
   };
 };
 
